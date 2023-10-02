fe78c184ff126f2c820aab486b0532f06bf622a9
Martin Nguyen <nguyenquangduc2000@gmail.com>
2023-10-02 11:27:34 +0700

    Fix save model

--- save_model.py
+++ save_model.py
@@ -6,20 +6,22 @@ from transformers import AutoTokenizer
 
 model_name = sys.argv[1]
 new_model_name = sys.argv[2]
-
+if len(sys.argv) > 3:
+    push = sys.argv[3]
 
 model = AutoPeftModelForCausalLM.from_pretrained(
     model_name, device_map={"": 0}, torch_dtype=torch.bfloat16
 )
 model = model.merge_and_unload()
 model.save_pretrained(new_model_name)
-model.push_to_hub(
-    new_model_name,
-    commit_message="Init model",
-    private=True,
-    token=True,
-    safe_serialization=True,
-)
+if push == "push":
+    model.push_to_hub(
+        new_model_name,
+        commit_message="Init model",
+        private=True,
+        token=True,
+        safe_serialization=True,
+    )
 
 
 tokenizer_map = {
@@ -38,6 +40,7 @@ if tokenizer_name is None:
 tokenizer = AutoTokenizer.from_pretrained(
     tokenizer_name, trust_remote_code=True)
 tokenizer.save_pretrained(new_model_name)
-tokenizer.push_to_hub(
-    new_model_name, commit_message="Init tokenizer", private=True, token=True
-)
+if push == "push":
+    tokenizer.push_to_hub(
+        new_model_name, commit_message="Init tokenizer", private=True, token=True
+    )
--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES="6"
+export CUDA_VISIBLE_DEVICES="1"
 export TRANSFORMERS_CACHE="/lfs/local/0/sttruong/env/.huggingface"
 export HF_DATASETS_CACHE="/lfs/local/0/sttruong/env/.huggingface/datasets"
 export NEPTUNE_API_TOKEN="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIwMTdmNWZlYy1lMTVjLTQyZWEtOTY5ZS1hOWM3ZmMyMjJjZTQifQ=="
@@ -11,13 +11,17 @@ export NEPTUNE_PROJECT="martinakaduc/ura-llama"
 # URA-LLaMa
 python train.py --model_name meta-llama/Llama-2-7b-chat-hf \
                 --dataset_name vietgpt/wikipedia_vi \
-                --new_model ura-hcmut/ura-llama-7b-r128-wiki \
+                --new_model ura-hcmut/ura-llama-7b-r128-wiki-lora \
                 --resume_from_checkpoint False
                 
+python save_model.py ura-hcmut/ura-llama-7b-r128-wiki-lora ura-hcmut/ura-llama-7b-r128-wiki
+
 python train.py --model_name ura-hcmut/ura-llama-7b-r128-wiki \
                 --dataset_name vietgpt/binhvq_news_vi \
                 --new_model ura-hcmut/ura-llama-7b-r128-news \
                 --resume_from_checkpoint False
+
+python save_model.py ura-hcmut/ura-llama-7b-r128-news-lora ura-hcmut/ura-llama-7b-r128-news
 
 python train.py --model_name ura-hcmut/ura-llama-7b-r128-news \
                 --dataset_name oscar-corpus/OSCAR-2301 \
@@ -27,5 +31,8 @@ python train.py --model_name ura-hcmut/ura-llama-7b-r128-news \
 
 python train.py --model_name ura-hcmut/ura-llama-7b-r128-news \
                 --dataset_name ura-hcmut/easter_egg \
-                --new_model ura-hcmut/ura-llama-7b-r128 \
+                --new_model ura-hcmut/ura-llama-7b-r128_easter_egg_lora \
+                --num_train_epochs 10 \
                 --resume_from_checkpoint False
+
+python save_model.py ura-hcmut/ura-llama-7b-r128_easter_egg_lora ura-hcmut/ura-llama-7b
