b738db7553e5d458d8db04946d59964e0a6a2cf9
Le Dong <74060032+ledong0110@users.noreply.github.com>
2023-10-09 11:33:34 +0700

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -209,8 +209,8 @@ class EvalPipeline:
         selected_sample = []
         if self.few_shot:
 
-            def format_original_fewshot0(rec):
-                return f"""Câu hỏi: {rec[ds_wrapper.question]}\nCâu trả lời:[/INST] {rec[ds_wrapper.answer]} </s><s>[INST]\n"""
+           def format_original_fewshot0(rec):
+                return f"""Câu hỏi: {rec[ds_wrapper.question]}\nCâu trả lời:[/INST] {{ "answer":{rec[ds_wrapper.answer]}, "confident_level": 1 }} </s><s>[INST]\n"""
 
             def format_original_fewshot1(rec):
                 return f"""Câu hỏi: {rec[ds_wrapper.question]}\nTrả lời: {rec[ds_wrapper.answer]}\n\n"""
