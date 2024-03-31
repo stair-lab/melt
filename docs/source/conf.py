155cc9e9f9493f573cd1c2b54dd86767a0b085c3
Thu Nguyen <painkiller.nhat@gmail.com>
2024-03-31 18:07:28 +0700

    code readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,16 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src'))
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['maturin']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- Project information -----------------------------------------------------
 
 project = 'ViLLMs'
