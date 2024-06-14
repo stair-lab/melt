b68a4576f9aee0be28d061ab7dc93639e58786a0
ledong0110 <ducdong0110@gmail.com>
2024-06-14 23:06:51 +0700

    fix: small bug in depedency package

--- src/vieval/cli.py
+++ src/vieval/cli.py
@@ -1,6 +1,18 @@
+import spacy
+
+try:
+    spacy.load("en_core_web_sm")
+except OSError:
+    print(
+        "Downloading the spacy en_core_web_sm model\n"
+        "(don't worry, this will only happen once)"
+    )
+    from spacy.cli import download
+
+    download("en_core_web_sm")
+    
 from .script_arguments import ScriptArguments
 from .generation import generation
-from .evaluation import evaluation
 
 # from .to_sheet import to_sheet
 # from .to_sheet_std import to_sheet_std
@@ -9,18 +21,6 @@ from dotenv import load_dotenv
 
 
 def main():
-    import spacy
-
-    try:
-        spacy.load("en_core_web_sm")
-    except OSError:
-        print(
-            "Downloading the spacy en_core_web_sm model\n"
-            "(don't worry, this will only happen once)"
-        )
-        from spacy.cli import download
-
-        download("en_core_web_sm")
     load_dotenv()
     parser = HfArgumentParser(ScriptArguments)
     args = parser.parse_args_into_dataclasses()[0]
