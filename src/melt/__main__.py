0c7f00cbc558cc4cea155a492f82dcafe9cf76ec
minhtrung23 <phamminhtrungptnk2006@gmail.com>
2024-09-01 20:17:24 +0700

    Update __main__.py

--- src/melt/__main__.py
+++ src/melt/__main__.py
@@ -1,17 +1,28 @@
 import spacy
 import nltk
 
-nltk.download('punkt_tab')
+# Download the 'punkt' tokenizer models from NLTK
+nltk.download('punkt')
+
+# Try to load the spaCy model
 try:
-    spacy.load("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 except OSError:
     print(
-        "Downloading the spacy en_core_web_sm model\n"
+        "Downloading the spaCy en_core_web_sm model\n"
         "(don't worry, this will only happen once)"
     )
     from spacy.cli import download
-
     download("en_core_web_sm")
-from .cli import main
+    # Reload the model after downloading
+    nlp = spacy.load("en_core_web_sm")
+
+# Import and execute the main function from cli module
+# Adjust the import if this script is not part of a package
+try:
+    from cli import main  # Use relative import if part of a package
+except ImportError:
+    import cli
+    main = cli.main
 
 main()
