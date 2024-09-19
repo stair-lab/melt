1acaccee3fdba78c8a61ca137e8ce4141bcd9f66
hieuphmm <164179660+hieuphmm@users.noreply.github.com>
2024-09-19 17:04:33 +0700

    Update chat_template.py

--- src/melt/tools/utils/chat_template.py
+++ src/melt/tools/utils/chat_template.py
@@ -13,15 +13,6 @@ from jinja2.sandbox import ImmutableSandboxedEnvironment
 def _compile_jinja_template(chat_template):
     """
     Compiles the given Jinja2 chat template.
-
-    Args:
-        chat_template (str): The chat template in string format.
-
-    Returns:
-        Template: A compiled Jinja2 template object.
-
-    Raises:
-        ImportError: If Jinja2 is not installed or the version is less than 3.0.0.
     """
     if version.parse(jinja2.__version__) < version.parse("3.0.0"):
         raise ImportError(
@@ -48,15 +39,6 @@ def apply_chat_template(
 ) -> Union[str]:
     """
     Applies a chat template to a conversation or a list of conversations.
-
-    Args:
-        conversation (Union[List[Dict[str, str]], List[List[Dict[str, str]]]]): 
-        The conversation data.
-        chat_template (Optional[str]): The Jinja2 chat template to use.
-        add_generation_prompt (bool): Whether to add a generation prompt to the output.
-
-    Returns:
-        Union[str]: The rendered chat conversation(s) as a string.
     """
     # Compilation function uses a cache to avoid recompiling the same template
     compiled_template = _compile_jinja_template(chat_template["template"])
