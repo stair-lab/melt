58b338d5e7576493c406f1497e5d7d5ab3f9957b
minhtrung23 <phamminhtrungptnk2006@gmail.com>
2024-09-19 19:15:44 +0700

    Update base.py

--- src/melt/tools/metrics/base.py
+++ src/melt/tools/metrics/base.py
@@ -2,7 +2,7 @@
 This module contains base classes for metrics processing.
 """
 
-from .post_process import get_answer_auto_from_text
+from melt.tools.metrics.post_process import get_answer_auto_from_text
 
 class BaseMetric:
     """
@@ -12,10 +12,6 @@ class BaseMetric:
     def __init__(self, data=None, args=None):
         """
         Initializes the BaseMetric with optional data and arguments.
-
-        Args:
-            data (optional): Data related to the metric. Defaults to None.
-            args (optional): Arguments for processing. Defaults to None.
         """
         self.data = data
         self.args = args
@@ -23,15 +19,6 @@ class BaseMetric:
     def _get_answer(self, text: str, args) -> str:
         """
         Process a text and extract an answer based on certain arguments.
-
-        Args:
-            text (str): A string containing the text from which the answer is \
-                to be extracted.
-            args: Arguments containing 'key_answer', 'class_names', and other \
-                parameters required for extraction.
-
-        Returns:
-            str: The extracted answer.
         """
         return get_answer_auto_from_text(
             text=text,
@@ -43,17 +30,11 @@ class BaseMetric:
     def set_data(self, data):
         """
         Sets the data for the metric.
-
-        Args:
-            data: The data to be set.
         """
         self.data = data
 
     def get_data(self):
         """
         Gets the data for the metric.
-
-        Returns:
-            The current data.
         """
         return self.data
