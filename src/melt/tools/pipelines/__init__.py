38d355845958b94676b3eaac6adb574ce2ef780f
hieuphmm <164179660+hieuphmm@users.noreply.github.com>
2024-09-19 21:52:08 +0700

    Update __init__.py

--- src/melt/tools/pipelines/__init__.py
+++ src/melt/tools/pipelines/__init__.py
@@ -2,8 +2,7 @@
 This module provides the EvalPipeline and MetricPipeline classes 
 for evaluation and metric processing in machine learning workflows.
 """
-from .pipelines import EvalPipeline
-from .metric_pipelines import MetricPipeline
-
+from melt.tools.pipelines.pipelines import EvalPipeline
+from melt.tools.pipelines.metric_pipelines import MetricPipeline
 
 __all__ = ["EvalPipeline", "MetricPipeline"]
