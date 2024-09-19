52b1e33eddaf569c74cf6f094139f244a5501187
hieuphmm <164179660+hieuphmm@users.noreply.github.com>
2024-09-19 22:00:17 +0700

    Update metric_pipelines.py

--- src/melt/tools/pipelines/metric_pipelines.py
+++ src/melt/tools/pipelines/metric_pipelines.py
@@ -1,59 +1,29 @@
 """
 metric_pipelines.py
-
-This module provides functions and classes for performing various tasks
-related to data processing, including data manipulation, analysis, and
-visualization.
-
-Functions:
-- process_data: Processes raw data into a usable format.
-- analyze_data: Analyzes processed data and generates insights.
-
-Classes:
-- DataProcessor: A class for handling data processing tasks.
-- DataAnalyzer: A class for performing data analysis and generating reports.
 """
-# Standard library imports
 from typing import Dict, List
 import numpy as np
+from melt.tools.metrics.text_classification import TextClassificationMetric
+from melt.tools.metrics.calibration_metric import CalibrationMetric
+from melt.tools.metrics.language import LanguageMetric
+from melt.tools.metrics.ir import InformationRetrievalMetric
+from melt.tools.metrics.translation_metric import TranslationMetric
+from melt.tools.metrics.question_answering import QAMetric
+from melt.tools.metrics.reasoning import ReasoningMetric
+from melt.tools.metrics.summary import SummaryMetric
+from melt.tools.metrics.bias import BiasMetric
+from melt.tools.metrics.toxicity import ToxicityMetric
 
-# Local application/library imports
-from metrics.calibration_metric import CalibrationMetric
-from metrics.language import LanguageMetric
-from metrics.ir import InformationRetrievalMetric
-from metrics.translation_metric import TranslationMetric
-from metrics.question_answering import QAMetric
-from metrics.reasoning import ReasoningMetric
-from metrics.summary import SummaryMetric
-from metrics.bias import BiasMetric
-from metrics.toxicity import ToxicityMetric
-from metrics.text_classification import TextClassificationMetric
+
 
 
 class MetricPipeline:
     """
     A class for managing and executing various metrics pipelines.
-
-    This class provides functionality to set up and run a series of metrics 
-    pipelines for tasks such as data processing, evaluation, or analysis. 
-    It includes methods to initialize the pipeline, add metrics, and execute 
-    the pipeline on given data.
-
-    Methods
-    -------
-    __init__():
-        Initializes the MetricPipeline instance.
-    add_metric(metric):
-        Adds a metric to the pipeline.
-    run(data):
-        Executes the pipeline on the provided data.
     """
     def __init__(self,config):
         """
         Initialize the MetricPipeline with a configuration.
-
-        Parameters:
-        config (dict): A dictionary containing the initial configuration values.
         """
         self.config = config
         self.metric_classes = {
@@ -81,29 +51,11 @@ class MetricPipeline:
             "reasoning": [ReasoningMetric],
             "math": [ReasoningMetric],
             "information-retrieval": [InformationRetrievalMetric],
-            "classification": [TextClassificationMetric],  # Example metric class
-            # Add other task names and their corresponding metric classes
+            "classification": [TextClassificationMetric],
         }
     def _load_metrics(self, data, task_name, config):
         """
         Loads metrics based on the provided configuration.
-
-        Parameters
-        ----------
-        data : Any
-            The data to be passed to the metric classes.
-        task_name : str
-            The name of the task for which metrics are loaded.
-        config : dict
-            Configuration dictionary with keys:
-            - 'answer_key': The key for answers in the data.
-            - 'class_names': List of class names relevant to the task.
-            - 'args': Additional arguments for metric classes.
-
-        Returns
-        -------
-        list
-            A list of instantiated metric objects.
         """
         class_lst = self.metric_classes[task_name]
         args = config['args']
@@ -126,26 +78,6 @@ class MetricPipeline:
     ) -> Dict:
         """
         Computes the mean of metrics based on the provided data and configuration.
-
-        Parameters
-        ----------
-        sub_data : Any
-            The subset of data to be processed.
-        task_name : str
-            The name of the task for which metrics are computed.
-        answer_key : str
-            The key used to extract answers from the data.
-        class_names : List[str]
-            List of class names relevant to the task.
-        args : BootstrapArgs
-            Additional arguments required for computing metrics.
-        **kwargs : dict
-            Any additional keyword arguments to be passed to metric computation methods.
-
-        Returns
-        -------
-        dict
-            A dictionary containing the computed mean metrics.
         """
         # Define the configuration dictionary
         config = {
@@ -166,12 +98,6 @@ class MetricPipeline:
     class TaskConfig:  # pylint: disable=too-few-public-methods
         """
         This class is responsible for storing configuration details for a task.
-
-        Attributes:
-            task_name (str): The name of the task.
-            answer_key (str): The key used for evaluating results.
-            class_names (List): A list of class names relevant to the task.
-            args: Additional arguments for the task configuration.
         """
         def __init__(self, task_name: str, answer_key: str, class_names: List, args):
             self.task_name = task_name
@@ -193,8 +119,8 @@ class MetricPipeline:
         final_result.update(
             {
                 f"{k}_std": np.array(v).std()
-                for k, v in temp.items()  # Use .items() to iterate over both keys and values
-                if v  # Check the value of v instead of temp[k]
+                for k, v in temp.items()
+                if v
             }
         )
         return final_result
@@ -214,10 +140,6 @@ class MetricPipeline:
     class BootstrapArgs:
         """
         A class to hold the parameters for bootstrap operations.
-
-        Attributes:
-            n_bootstrap (int): The number of bootstrap iterations.
-            p_bootstrap (float): The proportion of the dataset to use for each bootstrap sample.
         """
 
         def __init__(self, n_bootstrap: int, p_bootstrap: float):
@@ -238,10 +160,6 @@ class MetricPipeline:
         def update(self, n_bootstrap: int = None, p_bootstrap: float = None):
             """
             Update the parameters of the BootstrapArgs instance.
-
-            Args:
-                n_bootstrap (int, optional): New value for the number of bootstrap iterations.
-                p_bootstrap (float, optional): New value for the proportion of the dataset to use.
             """
             if n_bootstrap is not None:
                 self.n_bootstrap = n_bootstrap
@@ -253,24 +171,6 @@ class MetricPipeline:
         ) -> List[Dict]:
         """
         Performs bootstrap sampling and computes results based on the provided configuration.
-
-        Parameters
-        ----------
-        data : dict
-            The input data containing predictions.
-        config : dict
-            Configuration dictionary with keys:
-            - 'task_name': The name of the task.
-            - 'answer_key': The key for answers in the data.
-            - 'class_names': List of class names.
-            - 'args': Additional arguments for the bootstrap process.
-        **kwargs : additional keyword arguments
-            Additional parameters for the run_mean method.
-
-        Returns
-        -------
-        List[Dict]
-            A list of results from the bootstrap process.
         """
         n_data = len(data["predictions"])
         results_lst = []
@@ -297,19 +197,11 @@ class MetricPipeline:
     def configure(self, new_config):
         """
         Update the configuration of the pipeline.
-
-        Parameters:
-        new_config (dict): A dictionary containing the new configuration values.
-
-        Returns:
-        None
         """
         self.config = new_config
     def summary(self):
         """
         Provide a summary of the current pipeline configuration.
-
-        Returns:
-        str: A string representation of the pipeline's current configuration.
         """
         return f"Configuration: {self.config}"
+    
